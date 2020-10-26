b99db9192baafaccbe65a76bdd40326a0dc6ebdf
mdominguez33 <mdominguez33@hotmail.com>
2020-10-26 13:58:51 -0300

    cambio version para ikeargentina

--- src/views/bodies/splash.js
+++ src/views/bodies/splash.js
@@ -68,7 +68,7 @@ export class splashScreen extends connect(store, MEDIA_CHANGE, SCREEN)(LitElemen
     render() {
         return html `
         <div id="cuerpo" @click=${this.proximo}>
-        <div id="version">v.1.1.6</div>
+        <div id="version">v.1.1.9</div>
         </div>
         `
     }
