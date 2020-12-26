bc53e3cbde24c98a80d1c14d2ee9d9f8690c6298
BrunoMon <brunomon@hotmail.com>
2020-12-26 18:37:19 -0300

    cambio icono

--- src/views/bodies/splash.js
+++ src/views/bodies/splash.js
@@ -9,98 +9,98 @@ import { isInLayout } from "../../redux/screens/screenLayouts";
 const MEDIA_CHANGE = "ui.media.timeStamp";
 const SCREEN = "screen.timeStamp";
 export class splashScreen extends connect(store, MEDIA_CHANGE, SCREEN)(LitElement) {
-	constructor() {
-		super();
-		this.hidden = true;
-		this.area = "body";
-		this.timeOut = 0;
-	}
+    constructor() {
+        super();
+        this.hidden = true;
+        this.area = "body";
+        this.timeOut = 0;
+    }
 
-	static get styles() {
-		return css`
-			:host {
-				display: grid;
-				justify-content: center;
-				align-items: center;
-				position: absolute;
-				top: 0rem;
-				left: 0rem;
-				height: 100%;
-				width: 100%;
-				background-color: var(--color-celeste);
-			}
-			:host([hidden]) {
-				display: none;
-			}
-			#cuerpo {
-				display: block;
-				height: 100%;
-				width: 90vmin;
-				background-image: var(--imagen-logo-splash);
-				background-repeat: no-repeat;
-				background-position: center;
-				background-size: 100%;
-			}
-			#version {
-				display: grid;
-				position: absolute;
-				top: 3vh;
-				left: 3vw;
-				color: var(--color-blanco);
-				font-size: var(--font-header-h2-size);
-				font-weight: var(--font-header-h2-weight);
-			}
-		`;
-	}
-	render() {
-		return html`
-			<div id="cuerpo" @click=${this.proximo}>
-				<div id="version">v.1.2.8</div>
-			</div>
-		`;
-	}
-	stateChanged(state, name) {
-		if (name == SCREEN || name == MEDIA_CHANGE) {
-			this.mediaSize = state.ui.media.size;
-			this.hidden = true;
+    static get styles() {
+        return css`
+            :host {
+                display: grid;
+                justify-content: center;
+                align-items: center;
+                position: absolute;
+                top: 0rem;
+                left: 0rem;
+                height: 100%;
+                width: 100%;
+                background-color: var(--color-celeste);
+            }
+            :host([hidden]) {
+                display: none;
+            }
+            #cuerpo {
+                display: block;
+                height: 100%;
+                width: 90vmin;
+                background-image: var(--imagen-logo-splash);
+                background-repeat: no-repeat;
+                background-position: center;
+                background-size: 100%;
+            }
+            #version {
+                display: grid;
+                position: absolute;
+                top: 3vh;
+                left: 3vw;
+                color: var(--color-blanco);
+                font-size: var(--font-header-h2-size);
+                font-weight: var(--font-header-h2-weight);
+            }
+        `;
+    }
+    render() {
+        return html`
+            <div id="cuerpo" @click=${this.proximo}>
+                <div id="version">v.1.2.9</div>
+            </div>
+        `;
+    }
+    stateChanged(state, name) {
+        if (name == SCREEN || name == MEDIA_CHANGE) {
+            this.mediaSize = state.ui.media.size;
+            this.hidden = true;
 
-			const haveBodyArea = isInLayout(state, this.area);
-			const SeMuestraEnUnasDeEstasPantallas = "-splash-".indexOf("-" + state.screen.name + "-") != -1;
-			if (haveBodyArea && SeMuestraEnUnasDeEstasPantallas) {
-				this.hidden = false;
+            const haveBodyArea = isInLayout(state, this.area);
+            const SeMuestraEnUnasDeEstasPantallas = "-splash-".indexOf("-" + state.screen.name + "-") != -1;
+            if (haveBodyArea && SeMuestraEnUnasDeEstasPantallas) {
+                this.hidden = false;
 
-				this.timeOut = setTimeout(() => {
-					store.dispatch(goNext());
-				}, 3000);
-			}
-			this.update();
-		}
-	}
+                this.timeOut = setTimeout(() => {
+                    store.dispatch(goNext());
+                }, 3000);
+            }
+            this.update();
+        }
+    }
 
-	proximo() {
-		clearTimeout(this.timeOut);
-		store.dispatch(goNext());
-	}
+    proximo() {
+        clearTimeout(this.timeOut);
+        store.dispatch(goNext());
+    }
 
-	static get properties() {
-		return {
-			mediaSize: {
-				type: String,
-				reflect: true,
-				attribute: "media-size",
-			},
-			layout: {
-				type: String,
-				reflect: true,
-			},
-			hidden: {
-				type: Boolean,
-				reflect: true,
-			},
-			area: {
-				type: String,
-			},
-		};
-	}
+    static get properties() {
+        return {
+            mediaSize: {
+                type: String,
+                reflect: true,
+                attribute: "media-size",
+            },
+            layout: {
+                type: String,
+                reflect: true,
+            },
+            hidden: {
+                type: Boolean,
+                reflect: true,
+            },
+            area: {
+                type: String,
+            },
+        };
+    }
 }
 window.customElements.define("splash-screen", splashScreen);
